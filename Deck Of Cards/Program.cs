5551d11f9ee66d5b4451d8e0878e55b4db63070f
Akron254 <aaronrono42@gmail.com>
2018-11-03 12:01:45 +0300

    Initial commit

---
+++ Deck Of Cards/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Deck_Of_Cards
+{
+    class Program
+    {
+        class card
+        {
+            private string[] names = new string[52];
+            public string this[int index]
+            { 
+                get { return names[index];       }
+                set { names[index] = value;              }
+            }
+        }
+        static void Main(string[] args)
+        {
+            
+            //declarations:
+            string title = "temp";
+
+            string[] suitArr = { "Clubs", "Spades", "Diamonds", "Hearts", };
+
+            string[] cardArr = { "Ace", "King", "Queen", "Jack", "10", "9", "8", "7", "6", "5", "4", "3", "2", };
+
+            int z = 0;
+            card c = new card(); // array of objects
+// create property of each card
+            for (int x = 0; x < 13; x++)
+            {
+                title = cardArr[x] + " of temp";
+                {}
+                for (int y = 0; y < 4; y++)
+                {
+                    title = title.Replace("temp", suitArr[y]); 
+                    c[z] = title;
+                    z++;
+                    title = title.Replace(suitArr[y], "temp");
+                }
+            }
+
+            for (int p = 0; p < 52; p++) // print each object
+            {
+                Console.WriteLine(c[p]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
